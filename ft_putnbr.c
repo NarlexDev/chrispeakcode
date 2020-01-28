76ecb6a1289789c1afd2290c833b1636ec673a0b
laperouse christopher <laperouse.christopher@gmail.com>
2020-01-28 15:58:57 +0100

    putnbr

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -1,38 +1,56 @@
 #include <unistd.h>
 #include <stdio.h>
 
+int dmillier(unsigned int nb)
+{
+    nb = nb / 10000;
+    nb = 48 + nb % 10;
+    write(1,&nb,1);
+}
+
+int millier(unsigned int nb)
+{
+    nb = nb / 1000;
+    nb = 48 + nb % 10;
+    write(1,&nb,1);
+}
+
+int centaine(unsigned int nb)
+{
+    nb = nb / 100;
+    nb = 48 + nb % 10;
+    write(1,&nb,1);
+}
+
+int dizaine(unsigned int nb)
+{
+    nb = nb / 10;
+    nb = 48 + nb % 10;
+    write(1,&nb,1);
+}
+
 void ft_putnbr(int nb)
 {
-    int dm;
-    int m;
-    int c;
-    int d;
-    int u;
+    if(nb < 0)
+    {    nb = nb + (-2 * nb);
+        write(1,"-",1);
+    }    
+    if(nb > 9999)
+        dmillier(nb);
+    if(nb > 999)
+        millier(nb);
+    if(nb > 99)
+        centaine(nb);
+    if(nb > 9)   
+        dizaine(nb); 
 
-    dm = nb / 10000;
-    dm = 48 + dm % 48;
-    write(1,&dm,1);
-
-    m = nb / 1000;
-    m = 48 + m % 10;
-    write(1,&m,1);
-
-    c = nb / 100;
-    c = 48 + c % 10;
-    write(1,&c,1);
-
-    d = nb / 10;
-    d = 48 + d % 10;
-    write(1,&d,1);
-    
-    u = 48 + nb % 10;
-    write(1,&u,1);
-  
+    nb = nb % 10 + 48;
+    write(1,&nb,1);   
 }
 
 
 
 int main()
 {
-    ft_putnbr(850);   
+    ft_putnbr(-15545);   
 }
