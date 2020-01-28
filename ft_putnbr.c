8b0269c886cb8836f1819f8ea61beb850df37e30
laperouse christopher <laperouse.christopher@gmail.com>
2020-01-28 15:26:20 +0100

    putnbr

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -1,30 +1,38 @@
 #include <unistd.h>
 #include <stdio.h>
 
-void ft_swap(char *str)
+void ft_putnbr(int nb)
 {
-    int i;
-    int nb;
-    int k;
-   
-    k = 0;
-    i = 0;
-    nb = 0;
-    while(str[i] != 0)
-    {
-        k= str[i] % 12;
-        nb = k + 48;
-        write(1,&nb,1);
-        i++;
-    }
+    int dm;
+    int m;
+    int c;
+    int d;
+    int u;
+
+    dm = nb / 10000;
+    dm = 48 + dm % 48;
+    write(1,&dm,1);
+
+    m = nb / 1000;
+    m = 48 + m % 10;
+    write(1,&m,1);
+
+    c = nb / 100;
+    c = 48 + c % 10;
+    write(1,&c,1);
+
+    d = nb / 10;
+    d = 48 + d % 10;
+    write(1,&d,1);
+    
+    u = 48 + nb % 10;
+    write(1,&u,1);
+  
 }
 
 
 
 int main()
 {
-    int j = 50;
-    char *str;
-    str = "2498";
-    ft_swap(str);   
+    ft_putnbr(850);   
 }
